002dd13d4d63e11427cca0d4aed9330050937a00
KolesarAndreja <kolesar.andreja@gmail.com>
2020-06-18 09:29:01 +0200

    main menu and validation class commit

--- DAN_XXXIV_AndrejaKolesar/Program.cs
+++ DAN_XXXIV_AndrejaKolesar/Program.cs
@@ -2,14 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DAN_XXXIV_AndrejaKolesar
 {
     class Program
     {
+        public static void AppMethod()
+        {
+            Console.Write("Enter number of persons for first cash machine");
+            int a = Validation.ValidPositiveNumber();
+
+            Console.Write("Enter number of persons for second cash machine");
+            int b = Validation.ValidPositiveNumber();
+
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
+            string s = "";
+            Console.Write("1.Enter application \n2.Exit application \nYour choice: ");
+            do
+            { 
+                s = Console.ReadLine();
+                switch (s)
+                {
+                    case "1":
+                        Console.Write("1.Enter application \n2.Exit application \nYour choice: ");
+                        AppMethod();
+                        break;
+                    case "2":
+                        break;
+                    default:
+                        Console.Write("Invalid input. Try again:");
+                        break;
+                }
+            } while (s!="2");
         }
     }
 }
